bce855f376467e6ff95c406b61dcc82b2bbffcd7
Joaquin Magneres <joaquin.magneres@cymait.com>
2016-01-22 00:31:19 -0300

    delete react select

--- app/components/tracker/Lookup.jsx
+++ app/components/tracker/Lookup.jsx
@@ -1,7 +1,6 @@
 'use strict';
 
 import React from 'react';
-import Select from 'react-select';
 import Label from '../common/Label.jsx';
 import Button from '../common/Button.jsx';
 import Input from '../common/Input.jsx';
@@ -35,7 +34,6 @@ export default React.createClass({
         <div className="mdl-card__supporting-text">
           <fieldset>
             <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
-              <Select ref="stateSelect" autofocus options={options} simpleValue clearable={false} name="selected-company" value={this.state.selectValue} onChange={this.updateValue} searchable={true} />
               <Label htmlFor="trackingNumber" text="# Tracking" />
               <Input name="username" type="text" required={true}/>
             </div>

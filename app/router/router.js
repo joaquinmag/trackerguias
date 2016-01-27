144de57aa6c25d96d55200afaa3e597962bb8970
Joaquin Magneres <joaquin.magneres@cymait.com>
2016-01-27 19:15:27 -0300

    returning call to client

--- app/router/router.js
+++ app/router/router.js
@@ -14,11 +14,12 @@ export default function (app) {
   app.post('/tracker', function (req, res) {
     let courier = req.body.courier;
     let trackingNumber = req.body.trackingNumber;
-    trackingService.trackPackage(courier, trackingNumber).then(
+    trackingService.trackPackage(courier, trackingNumber)
+    .then(
       (data) => {
-        stream.debug(data.body.json);
+        stream.debug(data);
         res.json({
-          htmlResponse: JSON.stringify(data.body.json)
+          htmlResponse: JSON.stringify(data)
         });
       })
     .catch((err) => {
--- app/serverServices/trackingService.js
+++ app/serverServices/trackingService.js
@@ -2,22 +2,16 @@
 //const trackingRepository = require('../data/' + config.db + '/persistence/trackingRepository');
 import when from 'when';
 import easysoap from 'easysoap';
+import {stream} from '../util/logger';
+import _ from 'lodash';
 // headers: {
 //   'SOAPAction': '#Oca_e_Pak/Tracking_Pieza'
 // }
 
-const clientParams = {
-  'oca': {
-    host: 'webservice.oca.com.ar',
-    path: '/epak_tracking/Oep_TrackEPak.asmx',
-    wsdl: '/epak_tracking/Oep_TrackEPak.asmx?WSDL'
-  }
-};
-
-export default {
-  trackPackage(courier, trackingData) {
-    let soapClient = easysoap.createClient(clientParams[courier]);
-    return when(soapClient.call({
+const callSoapCourier = function (callSettings) {
+  let soapClient = easysoap.createClient(callSettings);
+  return when.promise((resolve, reject) => {
+    soapClient.call({
       method: 'Tracking_Pieza',
       attributes: {
         xmlns: '#Oca_e_Pak'
@@ -25,6 +19,39 @@ export default {
       params: {
         Pieza: '1808200000003582106'
       }
-    }));
+    })
+    .then((result) => {
+      if (!result) {
+        reject(new Error("result should be initialized"));
+      } else {
+        resolve(result.data);
+      }
+    })
+    .catch((err) => {
+      reject(err);
+    });
+  });
+};
+
+const clientParams = [
+  {
+    courier: 'oca',
+    callClient: callSoapCourier,
+    params: {
+      host: 'webservice.oca.com.ar',
+      path: '/epak_tracking/Oep_TrackEPak.asmx',
+      wsdl: '/epak_tracking/Oep_TrackEPak.asmx?WSDL'
+    }
+  }
+];
+
+export default {
+  trackPackage(courier, trackingData) {
+    let courierSettings = _.findWhere(clientParams, { 'courier': courier });
+    if (!courierSettings) {
+      throw new Error("courierSettings not available");
+    }
+    stream.debug(`courierSettings: ${JSON.stringify(courierSettings)}`);
+    return courierSettings.callClient(courierSettings.params);
   }
 };
